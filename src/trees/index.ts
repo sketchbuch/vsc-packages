d3b528d9f8e5052e054fb1cd22344426f9a6c356
SketchBuch <sb.design.de@gmail.com>
2019-12-01 07:19:47 +0100

    update linter commands

--- src/trees/index.ts
+++ src/trees/index.ts
@@ -1,2 +1,2 @@
-export { default as PackageList }from './PackageList/PackageList';
-export { default as PackageListItem }from './PackageList/PackageListItem';
+export { default as PackageList } from './PackageList/PackageList';
+export { default as PackageListItem } from './PackageList/PackageListItem';
