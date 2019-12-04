0e0470bcce9f6de33f229bada489fb8ebd191985
SketchBuch <sb.design.de@gmail.com>
2019-12-04 22:16:21 +0100

    updated package test

--- src/test/webviews/Package/Package.test.ts
+++ src/test/webviews/Package/Package.test.ts
@@ -1,3 +1,4 @@
+import * as sinon from 'sinon';
 import { expect } from 'chai';
 import { CMD_VSCODE_OPEN_WV } from '../../../constants';
 import { Package } from '../../../webviews';
@@ -5,9 +6,29 @@ import { mockContext, mockPanel, packageName } from '../../mocks';
 
 suite('Package()', () => {
   test('Creating an instance is successful', () => {
-    new Package(packageName, mockPanel, mockContext);
-    //expect(Package.currentPanel).to.equal(mockPanel);
-    expect(Package.currentPackage).to.equal('');
-    //expect(Package.viewType).to.equal(CMD_VSCODE_OPEN_WV);
+    expect(() => new Package(packageName, mockPanel, mockContext)).not.to.throw();
+  });
+
+  suite('constructor()', () => {
+    test('Public properties are set to defaults', () => {
+      new Package(packageName, mockPanel, mockContext);
+
+      expect(Package.currentPanel).to.equal(undefined);
+      expect(Package.currentPackage).to.equal('');
+      expect(Package.viewType).to.equal(CMD_VSCODE_OPEN_WV);
+    });
+
+    test('Calls panel.onDidDispose() && panel.onDidChangeViewState()', () => {
+      const spyOnDidChangeViewState = sinon.spy(mockPanel, 'onDidChangeViewState');
+      const spyOnDidDispose = sinon.spy(mockPanel, 'onDidDispose');
+
+      new Package(packageName, mockPanel, mockContext);
+
+      sinon.assert.calledOnce(spyOnDidChangeViewState);
+      sinon.assert.calledOnce(spyOnDidDispose);
+
+      spyOnDidChangeViewState.restore();
+      spyOnDidDispose.restore();
+    });
   });
 });
